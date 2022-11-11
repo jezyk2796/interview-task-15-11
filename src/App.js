892f5aa3d154aee425018c9646980b3638c44c2e
Szymon <jezyk2796@op.pl>
2022-11-11 23:35:06 +0100

    further setup. Static HTML created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 function App() {
   return (
     <div>
-      <h1>hello world</h1>
+      <h1>ATM Machine</h1>
+      <div>
+        <h3>Your money: {'test value'}</h3>
+      </div>
+      <div>
+        <button>Withdraw cash</button>
+        <button>Deposit money</button>
+      </div>
+      <div>
+        <input type="text" />
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+        </div>
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+        </div>
+        <div>
+          <button>0</button>
+          <button>Backspace</button>
+          <button>Clear input</button>
+        </div>
+        <div>
+          <button>Cancel</button>
+          <button>Accept</button>
+        </div>
+      </div>
     </div>
   );
 }
