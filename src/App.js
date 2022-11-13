aa044a18e864e258c6e7e97db30f3214f168dea2
Szymon <jezyk2796@op.pl>
2022-11-13 02:50:53 +0100

    changing views functionality added. Deposit and withdraw functions improved

--- src/App.js
+++ src/App.js
@@ -5,38 +5,44 @@ import ActionButton from './components/ActionButton.js'
 import KeyboardButton from './components/KeyboardButton.js'
 
 function App() {
+  const [currentView, setView] = useState('welcome');
   const [moneyAmount, setMoneyAmount] = useState(0);
   const [inputValue, setInputValue] = useState('');
 
 
-  const onWithdraw = () => {
-    console.log('withdraw')
-  }
+  const onWithdraw = () => setView('withdraw');
 
-  const onDeposit = () => {
-    console.log('deposit')
-  }
+  const onDeposit = () => setView('deposit');
 
-  const onNumberClick = keyValue => {
-    setInputValue(prevValue => prevValue += keyValue)
-  }
+  const onNumberClick = keyValue => setInputValue(prevValue => prevValue += keyValue);
 
   const onBackspaceClick = () => {
     const newValue = inputValue.slice(0, inputValue.length - 1);
     setInputValue(newValue);
   }
 
-  const onClearClick = () => {
-    setInputValue('');
-  }
+  const onClearClick = () => setInputValue('');
 
-  const onAccept = () => {
-    const inputValue = document.querySelector('input').value;
-
+  const onAcceptDeposit = () => {
     if (inputValue === '') {
+      // TODO alert
       return null;
     } else {
       setMoneyAmount(prevAmount => prevAmount + parseInt(inputValue));
+      setInputValue('');
+    }
+  }
+
+  const onAcceptWithdraw = () => {
+    if (inputValue === '') {
+      // TODO alert
+      return null;
+    } else {
+      if (moneyAmount - inputValue < 0) {
+        // TODO alert
+      } else {
+        setMoneyAmount(prevAmount => prevAmount - parseInt(inputValue));
+      }
       setInputValue('');
     }
   }
@@ -47,48 +53,55 @@ function App() {
       <div>
         <h3>Your money: { moneyAmount }</h3>
       </div>
-      <div>
-        <ActionButton
-          actionName='Withdraw cash'
-          onClick={onWithdraw}
-        />
-        <ActionButton
-          actionName='Deposit money'
-          onClick={onDeposit}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          readOnly
-          value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
-        />
+      {currentView === 'welcome' ?
         <div>
-          <KeyboardButton value="1" onClick={() => onNumberClick(1)} />
-          <KeyboardButton value="2" onClick={() => onNumberClick(2)} />
-          <KeyboardButton value="3" onClick={() => onNumberClick(3)} />
+          <ActionButton
+            actionName='Withdraw cash'
+            onClick={onWithdraw}
+          />
+          <ActionButton
+            actionName='Deposit money'
+            onClick={onDeposit}
+          />
         </div>
+        : null
+      }
+      {currentView !== 'welcome' ?
         <div>
-          <KeyboardButton value="4" onClick={() => onNumberClick(4)} />
-          <KeyboardButton value="5" onClick={() => onNumberClick(5)} />
-          <KeyboardButton value="6" onClick={() => onNumberClick(6)} />
+          <input
+            type="text"
+            readOnly
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+          />
+          <div>
+            <KeyboardButton value="1" onClick={() => onNumberClick(1)} />
+            <KeyboardButton value="2" onClick={() => onNumberClick(2)} />
+            <KeyboardButton value="3" onClick={() => onNumberClick(3)} />
+          </div>
+          <div>
+            <KeyboardButton value="4" onClick={() => onNumberClick(4)} />
+            <KeyboardButton value="5" onClick={() => onNumberClick(5)} />
+            <KeyboardButton value="6" onClick={() => onNumberClick(6)} />
+          </div>
+          <div>
+            <KeyboardButton value="7" onClick={() => onNumberClick(7)} />
+            <KeyboardButton value="8" onClick={() => onNumberClick(8)} />
+            <KeyboardButton value="9" onClick={() => onNumberClick(9)} />
+          </div>
+          <div>
+            <KeyboardButton value="0" onClick={() => onNumberClick(0)} />
+            <KeyboardButton value="Backspace" onClick={onBackspaceClick} />
+            <KeyboardButton value="Clear input" onClick={onClearClick} />
+          </div>
+          <div>
+            <button onClick={() => setView('welcome')}>Return</button>
+            {currentView === 'deposit' ? <button onClick={onAcceptDeposit}>Accept</button> : null}
+            {currentView === 'withdraw' ? <button onClick={onAcceptWithdraw}>Accept</button> : null}
+          </div>
         </div>
-        <div>
-          <KeyboardButton value="7" onClick={() => onNumberClick(7)} />
-          <KeyboardButton value="8" onClick={() => onNumberClick(8)} />
-          <KeyboardButton value="9" onClick={() => onNumberClick(9)} />
-        </div>
-        <div>
-          <KeyboardButton value="0" onClick={() => onNumberClick(0)} />
-          <KeyboardButton value="Backspace" onClick={onBackspaceClick} />
-          <KeyboardButton value="Clear input" onClick={onClearClick} />
-        </div>
-        <div>
-          <button>Cancel</button>
-          <button onClick={onAccept}>Accept</button>
-        </div>
-      </div>
+        : null
+      }
     </div>
   );
 }
